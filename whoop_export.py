f4e56310f6639b255a01b59aad4e604ec1309e39
Alix <alixakhribi@yahoo.com>
2021-01-03 18:46:06 -0800

    Grab user id and access token to return whoop user data from request

--- whoop_export.py
+++ whoop_export.py
@@ -23,3 +23,38 @@ if response.status_code != 200:
     exit()
 else:
     print("Success - Credentials accepted")
+
+# Set userid/token variables
+userid = response.json()['user']['id']
+access_token = response.json()['access_token']
+
+#################################################################
+# GET DATA
+
+# Download data
+url = 'https://api-7.whoop.com/users/{}/cycles'.format(userid)
+
+params = {
+    'start': '2021-01-03T00:00:00.000Z',
+    'end': '2021-01-03T00:00:00.000Z'
+}
+
+headers = {
+    'Authorization': 'bearer {}'.format(access_token)
+}
+
+response = requests.get(url, params=params, headers=headers)
+
+# Check if user/auth are accepted
+if response.status_code != 200:
+    print("Fail - User ID / auth token rejected.")
+    exit()
+else:
+    print("Success - User ID / auth token accepted")
+
+#################################################################
+# PARSE/TRANSFORM DATA
+
+# Convert data to json
+data_raw = response.json()
+print(data_raw)
